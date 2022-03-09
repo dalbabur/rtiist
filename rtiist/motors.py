2f53ede303e8af94dc6ab79457ddbc46a4e16d1d
DiegoAlba_Pi <dalba@uw.edu>
2022-03-08 17:42:56 -0800

    filterwheel

--- rtiist/motors.py
+++ rtiist/motors.py
@@ -1,3 +1,4 @@
+import time
 import board
 print(board.board_id)
 
@@ -25,8 +26,9 @@ class FilterWheel:
 
         self.first_sensor = dio.DigitalInOut(first_channel)
         self.first_sensor.direction = dio.Direction.INPUT
-        self.first_sensor.pull = dio.Pull.UP
-
+        self.first_sensor.pull = dio.Pull.UP # True when off
+        print(self.aligned_sensor.value)
+        print(self.first_sensor.value)
         self._find_first()
 
     def _counter(self):
@@ -34,16 +36,28 @@ class FilterWheel:
         if self.current_pos > self.filter_N:
             self.current_pos = 0
 
-    def _next(self): # could go fwd and rev, probs not worth it
-        while not self.aligned_sensor.value:
+    def _next(self, counter = True, reset = True): # could go fwd and rev, probs not worth it
+        self.motor.throttle = 1
+        time.sleep(2)
+        print('moving')
+        while self.aligned_sensor.value:
             self.motor.throttle = 1
         self.motor.throttle = 0
-        self._counter()
+        print('stop')
+        if counter: self._counter()
 
     def _find_first(self): # time it just in case it goes on forever 
-        while not self.first_sensor.value:
-            self._next()
-        self.current_pos = 0
+        for i in range(self.filter_N):
+            print(i)
+            self._next(counter = False)
+            if not self.first_sensor.value:
+                self.current_pos = 0
+                print('found first')
+                break
+            elif i+1 == self.filter_N:
+                print('Error')
+
+        self.motor.throttle = 0
 
     def set_filter(self, filter):
         # if filter not in self.filter_list: # or try excecpt
